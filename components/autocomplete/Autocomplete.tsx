98547d816ca8366d7cc202649e5a0d0457a194b9
Nikola Medarevic <nikolamedarevic@frontiercargroup.com>
2021-04-17 22:07:19 +0200

    Adds result item list

--- components/autocomplete/Autocomplete.tsx
+++ components/autocomplete/Autocomplete.tsx
@@ -1,14 +1,47 @@
-const defaultPlaceholder = 'Type something here'
-const defaultLabel = 'Find something'
+export const defaultPlaceholder = 'Type something here'
+export const defaultLabel = 'Find something'
+
+type ResultItem = {
+  id: string;
+  value: string;
+}
 
 interface AutocompleteProps {
-  placeholder: string;
-  label: string;
+  placeholder?: string;
+  label?: string;
+  resultItems?: ResultItem[];
+  ResultItemRenderer?: Function;
+}
+
+export const RenderString = ({item}: {item: ResultItem}) => (
+  <span data-testid={item.id}>{item.value}</span>
+)
+interface ResultListProps {
+  resultItems?: ResultItem[];
+  ResultItemRenderer?: Function;
+}
+const ResultList = ({
+  resultItems = [],
+  ResultItemRenderer = RenderString
+}: ResultListProps) => {
+  if (resultItems.length === 0) {
+    return null
+  }
+
+  return (
+    <div data-testid='result-list'>
+      {resultItems.map((item: ResultItem) => (
+        <ResultItemRenderer key={item.id} item={item} />
+      ))}
+    </div>
+  )
 }
 
 const Autocomplete = ({
   placeholder = defaultPlaceholder,
-  label = defaultLabel
+  label = defaultLabel,
+  resultItems = [],
+  ResultItemRenderer = RenderString
 }: AutocompleteProps) => {
   return (
     <div>
@@ -16,6 +49,10 @@ const Autocomplete = ({
       <div>
         <input id='query' type='text' placeholder={placeholder}/>
       </div>
+      <ResultList
+        resultItems={resultItems}
+        ResultItemRenderer={ResultItemRenderer}
+      />
     </div>
   )
 }
--- components/autocomplete/__tests__/Autocomplete.test.jsx
+++ components/autocomplete/__tests__/Autocomplete.test.jsx
@@ -3,9 +3,40 @@ import Autocomplete from '../Autocomplete'
 
 describe('<Autocomplete />', () => {
   it('should render', () => {
-    const {getByLabelText, getByPlaceholderText} = render(<Autocomplete />)
+    const {getByLabelText, getByPlaceholderText, queryAllByTestId} = render(<Autocomplete />)
 
     expect(getByLabelText('Find something')).toBeInTheDocument()
     expect(getByPlaceholderText('Type something here')).toBeInTheDocument()
+    expect(queryAllByTestId('render-string').length).toEqual(0)
+  })
+
+  it('should render a custom label', () => {
+    const {getByLabelText, getByPlaceholderText} = render(<Autocomplete label='Custom'/>)
+
+    expect(getByLabelText('Custom')).toBeInTheDocument()
+    expect(getByPlaceholderText('Type something here')).toBeInTheDocument()
+  })
+
+  it('should render a custom placeholder', () => {
+    const {getByLabelText, getByPlaceholderText} = render(<Autocomplete placeholder='Custom'/>)
+
+    expect(getByLabelText('Find something')).toBeInTheDocument()
+    expect(getByPlaceholderText('Custom')).toBeInTheDocument()
+  })
+
+  it('should render a custom placeholder', () => {
+    const {getByLabelText, getByPlaceholderText} = render(<Autocomplete placeholder='Custom'/>)
+
+    expect(getByLabelText('Find something')).toBeInTheDocument()
+    expect(getByPlaceholderText('Custom')).toBeInTheDocument()
+  })
+
+  it('should render a list of results', () => {
+    const results = [{id: 'someid', value: 'result1'}]
+    const {
+      getByText
+    } = render(<Autocomplete resultItems={results} />)
+
+    expect(getByText(results[0].value)).toBeInTheDocument()
   })
 })
